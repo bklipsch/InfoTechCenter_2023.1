08a9192a72534deb3136ce3b86c8de278a76104b
Binyam <02binyam.Klipsch@gulllakecs.org>
2023-10-23 05:27:41 -0700

    Add time library/variable for timeToSleep & Stable

--- ProjectOrangat.py
+++ ProjectOrangat.py
@@ -3,5 +3,12 @@ Our Welcome Screen will start our program letting
 drivers know that the InfoTech Center 2023 is loading
 """
 
+
+""''
+
+#Import Libraries Here        timeToSleep = 2
+import time
 print("\nWelcome - InfoTech Center 2023")
+timeToSleep = 2
+time.sleep(timeToSleep)
 print("\nInfoTech  Center 2023 is loading...")
