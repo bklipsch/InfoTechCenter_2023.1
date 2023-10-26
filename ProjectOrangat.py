9b88ac37d84dc016fb8fdd4df175de44cbe9d689
Binyam <02binyam.Klipsch@gulllakecs.org>
2023-10-26 05:25:04 -0700

    Add While Loop to load OS

--- ProjectOrangat.py
+++ ProjectOrangat.py
@@ -8,7 +8,26 @@ drivers know that the InfoTech Center 2023 is loading
 
 #Import Libraries Here        timeToSleep = 2
 import time
-print("\nWelcome - InfoTech Center 2023")
+import sys
+
 timeToSleep = 2
+
+print("\n\nWelcome - InfoTech Center 2023")
 time.sleep(timeToSleep)
-print("\nInfoTech  Center 2023 is loading...")
+
+#Add code to have the ellipsis add a dot every .5 seconds
+x = 0
+ellipsis = 0
+
+while x != 20:
+    x += 1
+    message = ("InfoTech  Center 2023 is loading" + "." * ellipsis)
+    ellipsis = ellipsis + 1
+    sys.stdout.write("\r" + message) # \r prints a carriage return first, so, message is printed on top of the previous line
+    time.sleep(.5)
+    if ellipsis == 4:
+        ellipsis = 0
+    if x == 20:
+        print("\n\nOperating System Loaded - Retina Scanned - Access Granted")
+
+
