c48dc12105ad3f91cd7221031d054b028813de01
Binyam <02binyam.Klipsch@gulllakecs.org>
2023-10-27 05:39:02 -0700

    Imported Libraries and Define a gasLevelGuage Function

---
+++ ProjectOrangat.py
@@ -0,0 +1,15 @@
+print("************************************************************************")
+print("Gasoline Branch\n\n")
+
+# Import Libraries Here
+import random
+from time import sleep
+
+def gasLevelGuage():
+    gasLevelList = ["Empty","Low","Quarter Tank","Half Tank","Three Quarter Tank","Full Tank"]
+    currentGasLevel = random.choice(gasLevelList)
+    return currentGasLevel
+
+
+(gasLevelGuage())
+
