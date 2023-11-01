628fc383f04cc4249e6da58cc197db35265123de
Binyam <02binyam.Klipsch@gulllakecs.org>
2023-11-01 04:58:11 -0700

    Add listOfGasStations() function

--- ProjectOrangat.py
+++ ProjectOrangat.py
@@ -11,7 +11,14 @@ def gasLevelGuage():
     currentGasLevel = random.choice(gasLevelList)
     return currentGasLevel
 
+# Function with a list of GasStations
+def listOfGasStation():
+    gasStations = ["Shell","Speedway","Exxon","Meijer","Costco","marathon","BP","Circle K","Westco"]
+    gasStationsNearby = random .choice(gasStations)
+    return gasStationsNearby
+
 #Function will call the gaslevelguage to determine gas level and find a close gas station if low
 def gasLevelAlert():
     milesToGasStationsLow = round(random.uniform(1,25),1)
     milesToGasStationsQuarterTank = round(random.uniform(25.1, 50), 1)
+
